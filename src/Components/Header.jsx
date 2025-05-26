0be41cfd8ba1097111c17aa3fb3aa5994906179a
AndreaChust <andrea.chust@hotmail.com>
2025-05-26 13:57:54 +0200

    progress

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,21 @@ function Header() {
     return (
         <header className="header">
             <div className="containerimg">
-                <img className="containerimg__logo" src={logoDeveloper} alt="" />
+                <a href=""><img className="containerimg__logo" src={logoDeveloper} alt="logo developer" /></a>
             </div>
-            <h1>Andrea Chust Ramos</h1>
+            <div>
+            <h1 className="header__title">Andrea Chust Ramos</h1>
+            <h2>Full Stack Developer en Murcia</h2>
+            </div>
+            <nav>
+                <ul className="list-header">
+                <a href=""><li>Inicio</li></a>
+                <a href=""><li>Sobre mí</li></a>
+                <a href=""><li>Linkdin</li></a>
+                <a href=""><li>GitHub</li></a>
+                </ul>
+
+            </nav>
         </header>
     )
 }
